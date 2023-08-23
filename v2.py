9de21e53d54bbcbde0fe61f3892a3c709fa36b20
Jim Wagner <jimvwagner@googlemail.com>
2023-08-23 22:46:32 +0200

    dockerized syncer and flask webhook for CI/CD

---
+++ v2.py
@@ -0,0 +1,19 @@
+import time
+import subprocess
+
+def main():
+    while True:
+        try:
+            # Run your script. Replace 'your_script.py' with your script's path.
+            subprocess.run(['python', 'sync.py'], check=True)
+            print("Synced!")
+            # Wait for 50 minutes (300 seconds)
+            time.sleep(300)
+            
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred while running the script: {e}")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+
+if __name__ == "__main__":
+    main()
